fc521425643d7bea76759208e5add8ae7864dab3
VAIBHAVVAEPE <vaibhavvarpe1997@gmail.com>
2022-06-07 08:13:17 +0530

    Add project files.

---
+++ Training Demo/ArrayEmployee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Demo
+{    //Write a program to initialize 5 employees details using array & display using foreach loop
+    class Employee
+    {
+        int id;
+        string name;
+        int age;
+      
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+       
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] Emplist = new Employee[5]
+            {
+                  
+                new Employee{Id=1,Name="Vaibhav",Age=22},
+                new Employee{Id=2,Name="Ram",Age=23},
+                new Employee{Id=3,Name="Ram",Age=25},
+                new Employee{Id=4,Name="Akshya",Age=22},
+                new Employee{Id=5,Name="Ajinkay",Age=21},
+            };
+
+            foreach(Employee aa in Emplist)
+            {
+                Console.WriteLine($"{aa.Id} {aa.Name} {aa.Age}");
+            }
+
+
+        }
+    }
+}
---
+++ Training Demo/ArrayProduct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Demo
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int Price { get; set; }
+
+    }
+    class AAA1
+    {
+        static void Main(string[] args)
+        {
+
+
+            Product[] productList = new Product[]
+            {
+                
+            new Product{Id=123,name="Parle biscuit",Price=5},
+            new Product{Id=685,name="Tigher biscuit",Price=6},
+            new Product{Id=858,name="Oreo biscuit",Price=10},
+            };
+
+            foreach(Product pp in productList)
+            {
+                Console.WriteLine($"{pp.Id} {pp.name}  {pp.Price} ");
+            }
+        }
+    }
+}
